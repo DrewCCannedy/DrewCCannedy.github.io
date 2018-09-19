edeb5fef68aa25f7200722b22fb54c92f10580f1
Drew Cannedy <dxc142830@utdallas.edu>
2018-09-19 12:18:46 -0500

    Changed Colors

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 $(document).ready(() => {
     $(".music").hover(() => {
-      $("body").css("background", "#DB3340");
+      $("body").css("background", "#990000");
       $(".music").css("color", "#ffffff");
       $("h1").css("color", "#ffffff");
     }, () => {
       $("body").css("background", "#ffffff");
-      $(".music").css("color", "#DB3340");
+      $(".music").css("color", "#990000");
       $("h1").css("color", "#000000");
     });
   
     $(".eternal-descent").hover(() => {
-      $("body").css("background", "#E8B71A");
+      $("body").css("background", "#4d004d");
       $(".eternal-descent").css("color", "#fff");
       $("h1").css("color", "#ffffff");
     }, () => {
       $("body").css("background", "#ffffff");
-      $(".eternal-descent").css("color", "#E8B71A");
+      $(".eternal-descent").css("color", "#4d004d");
       $("h1").css("color", "#000000");
     });
   
     $(".coding").hover(() => {
-      $("body").css("background", "#1FDA9A");
+      $("body").css("background", "#000066");
       $(".coding").css("color", "#fff");
       $("h1").css("color", "#ffffff");
     }, () => {
       $("body").css("background", "#ffffff");
-      $(".coding").css("color", "#1FDA9A");
+      $(".coding").css("color", "#000066");
       $("h1").css("color", "#000000");
     });
   
     $(".social").hover(() => {
-      $("body").css("background", "#28ABE3");
+      $("body").css("background", "#1aa3ff");
       $(".social").css("color", "#fff");
       $("h1").css("color", "#ffffff");
     }, () => {
       $("body").css("background", "#ffffff");
-      $(".social").css("color", "#28ABE3");
+      $(".social").css("color", "##1aa3ff");
       $("h1").css("color", "#000000");
     });
   });
