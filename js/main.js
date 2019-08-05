f602f19575ba1736a15fe311baacd089f5a1e9ea
Drew Cannedy <Drew.Cannedy@CenturyLink.com>
2019-08-05 17:20:24 -0500

    revamp of website

--- js/main.js
+++ js/main.js
@@ -1,41 +1,52 @@
-$(document).ready(() => {
-    $(".music").hover(() => {
-      $("body").css("background", "#990000");
-      $(".music").css("color", "#ffffff");
-      $("h1").css("color", "#ffffff");
-    }, () => {
-      $("body").css("background", "#ffffff");
-      $(".music").css("color", "#990000");
-      $("h1").css("color", "#000000");
-    });
+function changeSelection(selection) {
+  document.getElementById("links").remove();
+ 
+  var linkDiv = document.createElement("div");
+  linkDiv.setAttribute("id", "links");
+
+  var links = [];
   
-    $(".eternal-descent").hover(() => {
-      $("body").css("background", "#4d004d");
-      $(".eternal-descent").css("color", "#fff");
-      $("h1").css("color", "#ffffff");
-    }, () => {
-      $("body").css("background", "#ffffff");
-      $(".eternal-descent").css("color", "#4d004d");
-      $("h1").css("color", "#000000");
-    });
+  switch(selection) {
+    case "projects":
+      links.push(createLink("github", "https://github.com/DrewCCannedy"));
+      links.push(createLink("eternal-descent", "Eternal-Descent.zip"));
+      document.body.setAttribute("class", "project");
+      break;
+    case "music":
+      links.push(createLink("bandcamp", "https://drewcannedy.bandcamp.com"));
+      links.push(createLink("soundcloud", "https://soundcloud.com/drewcan"));
+      document.body.setAttribute("class", "music");
+      break;
+    case "social":
+      links.push(createLink("linkedin", "https://www.linkedin.com/in/drew-cannedy-87a6b6164/"));
+      document.body.setAttribute("class", "social");
+      break;
+  }
   
-    $(".coding").hover(() => {
-      $("body").css("background", "#000066");
-      $(".coding").css("color", "#fff");
-      $("h1").css("color", "#ffffff");
-    }, () => {
-      $("body").css("background", "#ffffff");
-      $(".coding").css("color", "#000066");
-      $("h1").css("color", "#000000");
-    });
-  
-    $(".social").hover(() => {
-      $("body").css("background", "#1aa3ff");
-      $(".social").css("color", "#fff");
-      $("h1").css("color", "#ffffff");
-    }, () => {
-      $("body").css("background", "#ffffff");
-      $(".social").css("color", "##1aa3ff");
-      $("h1").css("color", "#0");
-    });
+  links.forEach((link) => {
+    linkDiv.appendChild(link);
   });
+
+  var container = document.getElementById("container");
+  container.appendChild(linkDiv);
+}
+
+function changeSelectionProjects() {  
+  
+
+  changeSelection(links);
+}
+
+function createLink(content, href) {
+  var tempLink;
+  var tempH2;
+
+  tempH2 = document.createElement("h2");
+  tempH2.innerHTML = content;
+
+  tempLink = document.createElement("a");
+  tempLink.setAttribute("href", href);
+  tempLink.appendChild(tempH2);
+
+  return tempLink;
+}
