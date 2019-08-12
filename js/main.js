dda2cc213b5ffeb811f71a96fdf269eb8f595892
Drew Cannedy <Drew.Cannedy@CenturyLink.com>
2019-08-12 11:42:01 -0500

    adding eslint

--- js/main.js
+++ js/main.js
@@ -1,52 +1,44 @@
-function changeSelection(selection) {
-  document.getElementById("links").remove();
- 
-  var linkDiv = document.createElement("div");
-  linkDiv.setAttribute("id", "links");
+// eslint-disable-next-line no-unused-vars
+const changeSelection = (selection) => {
+  document.getElementById('links').remove()
 
-  var links = [];
-  
-  switch(selection) {
-    case "projects":
-      links.push(createLink("github", "https://github.com/DrewCCannedy"));
-      links.push(createLink("eternal-descent", "Eternal-Descent.zip"));
-      document.body.setAttribute("class", "project");
-      break;
-    case "music":
-      links.push(createLink("bandcamp", "https://drewcannedy.bandcamp.com"));
-      links.push(createLink("soundcloud", "https://soundcloud.com/drewcan"));
-      document.body.setAttribute("class", "music");
-      break;
-    case "social":
-      links.push(createLink("linkedin", "https://www.linkedin.com/in/drew-cannedy-87a6b6164/"));
-      document.body.setAttribute("class", "social");
-      break;
+  const linkDiv = document.createElement('div')
+  linkDiv.setAttribute('id', 'links')
+
+  const links = []
+
+  switch (selection) {
+    case 'projects':
+      links.push(createLink('github', 'https://github.com/DrewCCannedy'))
+      links.push(createLink('eternal-descent', 'resources/Eternal-Descent.zip'))
+      document.body.setAttribute('class', 'project')
+      break
+    case 'music':
+      links.push(createLink('bandcamp', 'https://drewcannedy.bandcamp.com'))
+      links.push(createLink('soundcloud', 'https://soundcloud.com/drewcan'))
+      document.body.setAttribute('class', 'music')
+      break
+    case 'social':
+      links.push(createLink('linkedin', 'https://www.linkedin.com/in/drew-cannedy-87a6b6164/'))
+      document.body.setAttribute('class', 'social')
+      break
   }
-  
+
   links.forEach((link) => {
-    linkDiv.appendChild(link);
-  });
+    linkDiv.appendChild(link)
+  })
 
-  var container = document.getElementById("container");
-  container.appendChild(linkDiv);
+  const container = document.getElementById('container')
+  container.appendChild(linkDiv)
 }
 
-function changeSelectionProjects() {  
-  
+const createLink = (content, href) => {
+  const tempH2 = document.createElement('h2')
+  tempH2.innerHTML = content
 
-  changeSelection(links);
-}
+  const tempLink = document.createElement('a')
+  tempLink.setAttribute('href', href)
+  tempLink.appendChild(tempH2)
 
-function createLink(content, href) {
-  var tempLink;
-  var tempH2;
-
-  tempH2 = document.createElement("h2");
-  tempH2.innerHTML = content;
-
-  tempLink = document.createElement("a");
-  tempLink.setAttribute("href", href);
-  tempLink.appendChild(tempH2);
-
-  return tempLink;
+  return tempLink
 }
