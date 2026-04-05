f956e67976d50fa85a749fcb3f6f9c7f9da2cadc
Drew Cannedy <dakillac@gmail.com>
2026-04-05 12:50:04 -0500

    dooby-engine

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const changeSelection = (selection) => {
   switch (selection) {
     case 'projects':
       links.push(createLink('github', 'https://github.com/DrewCCannedy'))
+      links.push(createLink('dooby-engine', 'dooby-engine/'))
       links.push(createLink('eternal-descent', 'resources/Eternal-Descent.zip'))
       document.body.setAttribute('class', 'project')
       break
